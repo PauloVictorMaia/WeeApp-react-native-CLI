5caad70bd895b459adb842f03fc5aa69acb241fe
Paulo <pauloaspinfa@gmail.com>
2020-09-26 14:38:41 -0300

    scrollview explore screen

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -3,6 +3,7 @@ import { View, Text, Image} from 'react-native';
 import styles from './styles';
 import WeeAppIcon from '../../assets/images/wee-icon-white-1.png';
 import SearchIcon from '../../assets/images/loupe.svg'
+import { ScrollView } from 'react-native-gesture-handler';
 
  function Explore () {
 
@@ -16,6 +17,44 @@ import SearchIcon from '../../assets/images/loupe.svg'
                 <Text style={styles.textContent}>explore possíveis destinos !</Text>
                 <SearchIcon width='30' height='30' fill='#7E2DC2'  />
             </View>
+
+            <ScrollView horizontal={true}>
+                <View>
+                    <Image source={WeeAppIcon} />
+                </View>
+
+                <View>
+                    <Image source={WeeAppIcon} />
+                </View>
+
+                <View>
+                    <Image source={WeeAppIcon} />
+                </View>
+
+                <View>
+                    <Image source={WeeAppIcon} />
+                </View>
+
+                <View>
+                    <Image source={WeeAppIcon} />
+                </View>
+
+                <View>
+                    <Image source={WeeAppIcon} />
+                </View>
+
+                <View>
+                    <Image source={WeeAppIcon} />
+                </View>
+
+                <View>
+                    <Image source={WeeAppIcon} />
+                </View>
+
+                <View>
+                    <Image source={WeeAppIcon} />
+                </View>
+            </ScrollView>
         </View>
   )
 };
