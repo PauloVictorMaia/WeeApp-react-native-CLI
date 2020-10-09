207097c0b7a5fc23538a681d6334c65cdc1b4c0a
Paulo <pauloaspinfa@gmail.com>
2020-10-09 11:09:40 -0300

    swiper home structure

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, Image} from 'react-native';
 import TopBar from '../../components/topbar';
+import Swiper from 'react-native-swiper';
+import CarnavalImg from '../../assets/images/carnaval.jpg'
+import styles from '../../components/topbar/styles';
 
 function Home() {
     return(
     <>
         <TopBar />
-        <View><Text>Home</Text></View>
+        <View style={styles.container}>
+            <Text>teste</Text>
+            <Swiper
+            style={{height: 200}}
+            autoplay={true}
+            >
+                <Image source={CarnavalImg} />
+                <Image source={CarnavalImg} />
+            </Swiper> 
+        </View>
     </>
     );
 };
